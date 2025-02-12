e781c79ce008677105e57690a015eaedcff553bc
suh1088 <suh1088@iCloud.com>
2025-02-12 21:37:49 +0900

    9251

--- Test.cpp
+++ Test.cpp
@@ -15,26 +15,47 @@ typedef long long ll;
 
 string a, b;
 
-int fill(int row, int col, vector<vector<int>> &v)
-{
-    if (row == 0 || col == 0)
-    {
-        return v[row][col] = 0;
-    }
-    else if (a[row] == b[col])
-    {
-        return v[row][col] = v[row - 1][col - 1] + 1;
-    }
-    else if (a[row] != b[col])
-    {
-        return v[row][col] = max(v[row][col - 1], v[row - 1][col]);
-    }
-    else
-    {
-        cout << "SOMETHING WRONG!!\n";
-        return -1;
-    }
-}
+// int fill(int row, int col, vector<vector<int>> v)
+// {
+//     if (row == 0 || col == 0)
+//     {
+//         return v[row][col] = 0;
+//     }
+//     else if (a[row] == b[col])
+//     {
+//         return v[row][col] = v[row - 1][col - 1] + 1;
+//     }
+//     else if (a[row] != b[col])
+//     {
+//         return v[row][col] = max(v[row][col - 1], v[row - 1][col]);
+//     }
+//     else
+//     {
+//         cout << "SOMETHING WRONG!!\n";
+//         return -1;
+//     }
+// }
+
+// int fill(int row, int col, vector<vector<int>> v)
+// {
+//     if (row == 0 || col == 0)
+//     {
+//         return 0;
+//     }
+//     else if (a[row] == b[col])
+//     {
+//         return v[row - 1][col - 1] + 1;
+//     }
+//     else if (a[row] != b[col])
+//     {
+//         return max(v[row][col - 1], v[row - 1][col]);
+//     }
+//     else
+//     {
+//         cout << "SOMETHING WRONG!!\n";
+//         return -1;
+//     }
+// }
 
 int main(void)
 {
@@ -43,24 +64,24 @@ int main(void)
 
     vector<vector<int>> v(a.size() + 1, vector<int>(b.size() + 1, 0));
 
-    v[0][0] = 0;
-
-    int last = 0;
-    for (int i = 0; i <= min(a.size(), b.size()); i++)
+    for (int i = 1; i <= a.size(); i++)
     {
-        // 행 먼저
-        for (int j = i; j <= b.size(); j++)
+        for (int j = 1; j <= b.size(); j++)
         {
-            last = fill(j, i, v);
-        }
-        // 다음 가로
-        for (int j = i + 1; j <= a.size(); j++)
-        {
-            last = fill(i, j, v);
+            if (a[i - 1] == b[j - 1])
+            {
+                v[i][j] = v[i - 1][j - 1] + 1;
+            }
+            else if (a[i - 1] != b[j - 1])
+            {
+                v[i][j] = max(v[i][j - 1], v[i - 1][j]);
+            }
+            // last = fill(j, i, v);
+            // v[j][i] = last;
         }
     }
 
-    cout << last;
+    cout << v[a.size()][b.size()];
 }
 
 /*
