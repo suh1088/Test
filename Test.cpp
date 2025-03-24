6dee051f376e7deb26381019c22c56046b8bf344
suh1088 <suh1088@iCloud.com>
2025-03-24 19:35:01 +0900

    5397

--- Test.cpp
+++ Test.cpp
@@ -4,54 +4,12 @@
 
 using namespace std;
 
-int TtoI(string time){
-    int It = 0;
-    It += stoi(time.substr(3,2));
-    It += stoi(time.substr(0,2)) * 60;
-    return It;
-}
 
-string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands) {
-    
-    int st = TtoI(op_start);
-    int en = TtoI(op_end);
-    int len = TtoI(video_len);
-    int po = TtoI(pos);
-    
-    for(int i = 0 ; i < commands.size(); i++){
-        if(po >= st && po <= en){
-            po = en;
-        }
-        if(commands[i] == "prev"){
-            po -= 10;
-            if(po < 0){
-                po = 0;
-            }
-        }
-        
-        else if(commands[i] == "next"){
-            po += 10;
-            if(po > len){
-                po = len;
-            }
-        }
-    }
-    
-    string answer = "";
-    
-    int min = po / 60;
-    if(min < 10) answer += "0";
-    answer += to_string(min);
-    
-    answer += ":";
-    
-    int sec = po % 60;
-    if(sec < 10) answer += "0";
-    answer += to_string(sec);
-    
-    return answer;
-}
 
 int main(void){
-    cout << solution("10:55", "00:05","00:15","06:55",{"prev","next","next"});
+    
 }
+
+/*
+6064
+*/
