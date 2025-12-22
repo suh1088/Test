4e3b3f700ad6a1351d37cc19bc6aa53fce5d036b
suh1088 <suh1088@iCloud.com>
2025-12-22 15:30:40 +0900

    save

--- Test.cpp
+++ Test.cpp
@@ -13,76 +13,61 @@
 using namespace std;
 typedef long long ll;
 
-int N, cnt = 0;
-vector<int> board; // 가능세계 (0 ~ N-1)
+vector<pair<int, int>> obj;
+vector<vector<int>> dp;
+int N, W;
 
-bool chkThreat(int r, int c)
-{ // 들어오는 좌표는 가장 아래있는 퀸
-    // 가로 -> 한 줄에 한분씩 계시니까 걍 무시
-    // 세로 -> 위에서부터 체크
-    for (int i = 0; i < r; i++)
-    {
-        if (board[i] == c)
-            return true;
-    }
-
-    // 대각 -> 양쪽 대각으로 체크
-    for (int i = r - 1; i >= 0; i--)
-    {
-        int r_ = c + (r - i);
-        int l = c - (r - i);
-        if (l >= 0 && r_ < N)
-        {
-            if (board[i] == l || board[i] == r_)
-                return true;
-        }
-        else if (l >= 0)
-        {
-            if (board[i] == l)
-                return true;
-        }
-        else if (r_ < N)
-        {
-            if (board[i] == r_)
-                return true;
-        }
-    }
-
-    return false;
-}
-
-int dig(int dep)
+int fill(int n, int w)
 {
-    if (dep > N - 1)
+    if (n == 0)
     {
-        cnt++;
-        return 1;
-    }
-
-    for (int i = 0; i < N; i++)
-    {
-        if (!chkThreat(dep, i))
+        if (w >= obj[n].first)
         {
-            board[dep] = i;
-            dig(dep + 1);
+            return obj[n].second;
+        }
+        else
+        {
+            return 0;
         }
     }
-    return 0;
+
+    if (w < obj[n].first)
+    {
+        return dp[w][n - 1];
+    }
+    else
+    {
+        return max(dp[w][n - 1], obj[n].second + dp[w - obj[n].first][n - 1]);
+    }
 }
 
 int main(void)
 {
-    cin >> N;
-    board.resize(N, 0);
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
 
-    dig(0);
+    cin >> N >> W;
 
-    cout << cnt;
+    dp.resize(W + 1, vector<int>(N, 0));
+
+    for (int i = 0; i < N; i++)
+    {
+        int a, b; // w and v
+        cin >> a >> b;
+        obj.push_back(make_pair(a, b));
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j <= W; j++)
+        {
+            dp[j][i] = fill(i, j);
+        }
+    }
+
+    cout << dp[W][N - 1];
 }
 
 /*
-9663
-
-한 열에 최소 하나의 여왕님이 계셔야 함 ㅇㅇ
-ㄴ 대충 비둘기집 어쩌구
+12865
 */
